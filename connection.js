eb745fbdc29297be9239e1bda798186e8e1b0cee
ShreyasP20 <luciferjones2015@gmail.com>
2023-04-20 14:29:16 +0530

    NODEJS

---
+++ connection.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+
+console.log('Get Connection...');
+
+var conn =  mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'fdbms'
+});
+
+
+conn.query("Select * from Chef", (err, result) => {
+    return console.log(result)
+});
